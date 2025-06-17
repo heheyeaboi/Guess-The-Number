1b956ed53ce23076ff2fce520d047b25784a1676
Riyansh Agarwal <riyanshagarwal3333@gmail.com>
2025-06-17 19:53:49 +0530

    Create Main_File.js

---
+++ Main_File.js
@@ -0,0 +1,22 @@
+let randomNum=Math.floor(Math.random()*100)
+console.log(randomNum)
+let guess=prompt("Guess a number between 0 and 100")    
+let chance=1
+while(guess!=randomNum){
+    if(guess<randomNum){
+        console.log("Too Low")
+
+        chance++
+        guess=prompt("Guess a number between 0 and 100")
+        
+        continue;
+   }else if(guess>randomNum){
+        console.log("Too High")
+        chance++  
+        guess=prompt("Guess a number between 0 and 100")
+        continue;  
+   }else if(guess==randomNum){
+        break;
+   }
+}
+console.log("You guessed",randomNum, "right in",chance,"chances")
